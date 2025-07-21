3540db97deb0d83dba77ba4b475ba51157cbed95
Sujal Bhaedwaj <168054817+deacon1105@users.noreply.github.com>
2025-07-21 13:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoList =document.getElementById('todo-list');
 
-let allTodos =[];
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit', function(e){
     e.preventDefault();
@@ -12,8 +13,13 @@ todoForm.addEventListener('submit', function(e){
     function addTodo(){
         const todoText = todoInput.value.trim();
         if(todoText.length>0){
-            allTodos.push(todoText);
+          const todoObject ={
+            text : todoText,
+            completed: false
+          }
+            allTodos.push(todoObject);
             updateTodoList();
+            saveTodos();
             todoInput.value="";
         }
     }
@@ -29,6 +35,7 @@ function updateTodoList(){
 function createTodoItem(todo, todoIndex){
     const todoId = "todo-" + todoIndex;
    const todoLI = document.createElement("li");
+   const todoText = todo.text;
    todoLI.className = "todo";
    todoLI.innerHTML = `
   <input type="checkbox" id="${todoId}" />
@@ -36,7 +43,7 @@ function createTodoItem(todo, todoIndex){
             ><svg fill ="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
           </label>
           <label for="${todoId}" class="todo-text"
-            >${todo}</label
+            >${todoText}</label
           >
           <button class="delete-btn">
             <img
@@ -48,10 +55,29 @@ function createTodoItem(todo, todoIndex){
    deleteButton.addEventListener("click", ()=>{
     deleteTodoItem(todoIndex);
    })
+
+   const checkbox = todoLI.querySelector("input");
+   checkbox.addEventListener("change", ()=>{
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+   })
+   checkbox.checked = todo.completed;
     return todoLI;
+}
+
+function saveTodos(){
+  const todosJson = JSON.stringify(allTodos);
+  localStorage.setItem("todos", todosJson);
+}
+
+function getTodos(){
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
 }
 
 function deleteTodoItem(todoIndex){
     allTodos = allTodos.filter((_, i) => i!== todoIndex);
+    saveTodos();
     updateTodoList();
 }
+
